380ac5d304e432a39e73081f520825749d0de0db
DJ Shitcoin™ <dj@shitcoin.jp>
2018-11-04 14:51:54 +0900

    Update vue-actioncable.js

--- vue-actioncable.js
+++ vue-actioncable.js
@@ -20,11 +20,17 @@ var VueActionCable = {
           if (typeof paramsFn !== 'function') {
             paramsFn = function() { return subOptions.params }
           }
+          var noop = function() {}
+          if (!subOptions.received) subOptions.received = noop;
+          if (!subOptions.connected) subOptions.connected = noop;
+          if (!subOptions.disconnected) subOptions.disconnected = noop;
           $vm.$watch(paramsFn, function(params) {
             if ($vm._subscriptions[channelName]) $vm._subscriptions[channelName].unsubscribe()
             params.channel = channelName
             $vm._subscriptions[channelName] = cable.subscriptions.create(params, {
-              received: subOptions.received.bind($vm)
+              received: subOptions.received.bind($vm),
+              connected: subOptions.connected.bind($vm),
+              disconnected: subOptions.disconnected.bind($vm)
             })
           }, {
             immediate: true
